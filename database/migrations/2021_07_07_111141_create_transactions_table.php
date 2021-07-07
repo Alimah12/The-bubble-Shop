3881d9895ceb7cdd803a5db9f4e13be87966b98f
SaiyaraOishi <saiyara.oishi@gmail.com>
2021-07-07 19:53:43 +0600

    React attached

---
+++ database/migrations/2021_07_07_111141_create_transactions_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->timestamps();
+            $table->integer('item_id');
+            $table->string('cloth_type');
+            $table->integer('quantity');
+            $table->decimal('cost',5,2);
+            
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
